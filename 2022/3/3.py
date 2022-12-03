3259a5ca39cbe4b8abb8c62f1337327b8977166a
mafaldalami <m.lami@campus.fct.unl.pt>
2022-12-03 17:29:08 +0000

    :sparkles: Day 3 - Part 2 (2022)

--- 2022/3/3.py
+++ 2022/3/3.py
@@ -1,3 +1,12 @@
+def cal_letter_value(letter: str) -> int:
+    val = 0
+    if letter.islower():
+        val += ord(letter) % 64 - 32
+    else:
+        val += ord(letter) % 64 + 26
+    return val
+
+
 def part_one(runsacks: list):
     sum_priorities = 0
     for runsack in runsacks:
@@ -6,16 +15,23 @@ def part_one(runsacks: list):
         compartment2 = runsack[split:]
         shared_items = set(compartment1).intersection(compartment2)
         for char in shared_items:
-            val = +ord(char) % 64
-            if char.islower():
-                sum_priorities += val - 32
-            else:
-                sum_priorities += val + 26
+            val = cal_letter_value(char)
+            sum_priorities += val
     return sum_priorities
 
 
-def part_two(plays: list):
-    pass
+def part_two(runsacks: list):
+    sum_priorities = 0
+    groups = [runsacks[i:i+3] for i in range(0, len(runsacks), 3)]
+    for group in groups:
+        g1 = group[0]
+        g2 = group[1]
+        g3 = group[2]
+        shared_items = set(g1).intersection(set(g2).intersection(g3))
+        for char in shared_items:
+            val = cal_letter_value(char)
+            sum_priorities += val
+    return sum_priorities
 
 
 def main():
@@ -26,7 +42,7 @@ def main():
     print(part_one(runsacks))
 
     print("---Part Two---")
-    # print(part_two(plays))
+    print(part_two(runsacks))
 
 
 if __name__ == "__main__":
